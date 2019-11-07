224f6de8ac4778295f5e3ffba1331906d76af9eb
Pradeep Agrawal <pradeep@systango.com>
2019-11-07 17:15:15 +0530

    removed authorization

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,17 +1,4 @@
 # frozen_string_literal: true
 
 class ApplicationController < Jets::Controller::Base
-  before_action :authorize
-
-  require 'base64'
-
-  private
-
-  def authorize
-    # c2VydmVybGVzcy1lbWFpbDpzZmRiMTI0YmI=
-    authorization_key = Base64.decode64(request.headers['authorization']) rescue nil
-    return if authorization_key == "#{ENV['API_CLIENT_ID']}:#{ENV['API_CLIENT_SECRET']}"
-
-    render json: { message: 'Unauthorized' }, status: 401
-  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,11 @@
+# frozen_string_literal: true
+
 Jets.application.routes.draw do
-  root "jets/public#show"
-  post "send_email", to: "emails#send_email"
+  root 'jets/public#show'
+  post 'email', to: 'emails#send_email'
   # The jets/public#show controller can serve static utf8 content out of the public folder.
   # Note, as part of the deploy process Jets uploads files in the public folder to s3
   # and serves them out of s3 directly. S3 is well suited to serve static assets.
   # More info here: http://rubyonjets.com/docs/assets-serving/
-  any "*catchall", to: "jets/public#show"
+  any '*catchall', to: 'jets/public#show'
 end
